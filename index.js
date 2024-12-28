76a81041d08888feba5db44819acc08e56dc92e3
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-28 17:37:41 -0500

    fix images

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// TODO: find permanent solution for image storage.
+const gitRepo = 'https://github.com/alexanderkazanski/Oldagram';
+
 const posts = [
     {
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: gitRepo + "images/avatar-vangogh.jpg",
+        post: gitRepo + "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -12,8 +15,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: gitRepo + "images/avatar-courbet.jpg",
+        post: gitRepo + "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,8 +24,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: gitRepo + "images/avatar-ducreux.jpg",
+        post: gitRepo + "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from "vite"
 
-export default defineConfig(() => ({
-  build: {
-    rollupOptions: {
-      input: [
-        './index.html',
-        './index.css',
-        './index.js',
-        './images/avatar-courbet.jpg',
-        './images/avatar-ducreux.jpg',
-        './images/avatar-vangogh.jpg',
-        './images/icon-comment.png',
-        './images/icon-dm.png',
-        './images/icon-heart.png',
-        './images/logo.png',
-        './images/post-courbet.jpg',
-        './images/post-ducreux.jpg',
-        './images/post-vangogh.jpg',
-        './images/user-avatar.png',
-      ],
-    },
-  },
-}));
+export default defineConfig({
+	plugins: [
+		
+	]
+})
