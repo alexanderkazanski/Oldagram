59e64f40d12da9acb736604562fbbdcf6ef4baf7
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-28 17:34:02 -0500

    fix images

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ export default defineConfig(() => ({
   build: {
     rollupOptions: {
       input: [
-        'index.html',
+        './index.html',
         './index.css',
         './index.js',
-        './images/*'
+        './images/avatar-courbet.jpg',
+        './images/avatar-ducreux.jpg',
+        './images/avatar-vangogh.jpg',
+        './images/icon-comment.png',
+        './images/icon-dm.png',
+        './images/icon-heart.png',
+        './images/logo.png',
+        './images/post-courbet.jpg',
+        './images/post-ducreux.jpg',
+        './images/post-vangogh.jpg',
+        './images/user-avatar.png',
       ],
     },
   },
